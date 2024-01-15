386b68e2ecf6c476e32d581200ecf6e71323519b
Scott Wilson <scott@Scotts-MacBook-Pro.local>
2024-01-15 06:45:04 -0600

    section 3 - lesson 45 - first commit

---
+++ 03-react-essentials/01-starting-project/src/App.jsx
@@ -0,0 +1,58 @@
+import reactImg from './assets/react-core-concepts.png';
+import { CORE_CONCEPTS } from './data.js';
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const description = reactDescriptions[getRandomInt(2)];
+
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are going to build!
+      </p>
+    </header>
+  );
+}
+
+function CoreConcept({image, title, description}) {
+  return (
+    <li>
+      <img src={image} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
+function CoreConceptsList({items}) {
+  return (
+    <ul>
+      {items.map(item => (
+        <CoreConcept {...item} />
+      ))}
+    </ul>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Time to get started!</h2>
+          <CoreConceptsList items={CORE_CONCEPTS} />
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
